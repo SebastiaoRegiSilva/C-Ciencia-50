622cc8c893a84ba4036bebfb3bb7e9b901b831e3
Sebastião Willian Pereira Regi <84387616+SebastiaoRegiSilva@users.noreply.github.com>
2023-09-10 01:19:40 +0000

    Sun, Sep 10, 2023, 1:19 AM +00:00

---
+++ C/Population/population.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+      // TODO: Solicite o valor inicial ao usuário
+
+     // TODO: Solicite o valor final ao usuário
+
+     // TODO: Calcule o número de anos até o limite
+
+     // TODO: Imprima o número de anos
+
+}
